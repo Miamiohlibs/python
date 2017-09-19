a80a897afc05756319307bd89cd78741d95d9e99
craigboman <craig.boman@gmail.com>
2017-09-19 08:32:43 -0400

    lots of stuff going on here

--- bib.py
+++ bib.py
@@ -1,24 +1,27 @@
 import get_token, requests, json
 from get_token import get_token
 
+
 def bib():
+    token_param = get_token()
 
-token_param = get_token()
+    url = "https://holmes.lib.miamioh.edu:443/iii/sierra-api/v4/bibs/marc?"
 
-url = "https://holmes.lib.miamioh.edu:443/iii/sierra-api/v4/bibs/marc?"
+    querystring = {"id": "4530689"} #full number is b4530689a minus b and final check digit a
 
-querystring = {"id":"4530689"}
-
-headers = {
-    'authorization': str(token_param),
-    'cache-control': "no-cache",
-    'postman-token': "715478e1-10c5-8bc7-7758-415c1be73131"
+    headers = {
+        'authorization': str(token_param),
+        'cache-control': "no-cache",
+        'postman-token': "715478e1-10c5-8bc7-7758-415c1be73131"
     }
 
-response = requests.request("GET", url, headers=headers, params=querystring)
+    response = requests.request("GET", url, headers=headers, params=querystring)
 
-bib_data = response.json()
+    bib_data = response.json()
 
-bib_url = bib_data["file"]
+    bib_url = bib_data["file"]
 
-marc_response = requests.request("GET", bib_url, headers=headers)
+    marc_response = requests.request("GET", bib_url, headers=headers)
+
+    return marc_response.text
+    #full_marc = marc_response.json()
