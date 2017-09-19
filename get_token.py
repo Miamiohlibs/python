0f6047877695d44f06a81a02e3787298da403d91
craigboman <craig.boman@gmail.com>
2017-09-19 08:34:34 -0400

    minus credentials

---
+++ get_token.py
@@ -0,0 +1,14 @@
+import json, requests, urllib3, get_bass, base64
+from get_bass import get_bass
+
+
+def get_token():
+    bass = get_bass()
+    url = 'https://holmes.lib.miamioh.edu:443/iii/sierra-api/v4/token'
+    payload = {'grant_type': 'client_credentials'}
+    headers = {'authorization': 'Basic ' + str(bass)}
+    r = requests.post(url, data=json.dumps(payload), headers=headers)
+    mytoken = r.json()['token_type']
+    mytokentype = r.json()['access_token']
+    token_param = str(mytoken) + ' ' + str(mytokentype)
+    return token_param
