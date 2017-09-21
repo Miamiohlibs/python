a34766c4b0b7c4b8bfdff227f35bc8a1055db69e
craigboman <craig.boman@gmail.com>
2017-09-21 14:58:44 -0400

    empty templates

---
+++ order.py
@@ -0,0 +1,6 @@
+import get_token, requests, json
+from get_token import get_token
+
+
+def order():
+    token_param = get_token()
---
+++ patron.py
@@ -0,0 +1,8 @@
+import get_token, requests, json
+from get_token import get_token
+
+
+#def patron():
+
+token_param = get_token()
+
