407686e62b370ec576f6095a764527fb515047d1
Craig Boman <craig.boman@gmail.com>
2017-12-06 15:46:49 -0500

    more stuff

--- bib.py
+++ bib.py
@@ -7,21 +7,21 @@ def bib():
 
     url = "https://holmes.lib.miamioh.edu:443/iii/sierra-api/v4/bibs/marc?"
 
-    querystring = {"id": "4530689"} #full number is b4530689a minus b and final check digit a
+    querystring = {"id": "4530689"}  # full number is b4530689a minus b and final check digit a
 
     headers = {
         'authorization': str(token_param),
         'cache-control': "no-cache",
-        'postman-token': "715478e1-10c5-8bc7-7758-415c1be73131"
+        'postman-token': "715478e1-10c5-8bc7-7758-415c1be73131",
+        'content-type': "application/json"
     }
 
-    response = requests.request("GET", url, headers=headers, params=querystring)
+    response = requests.get(url, headers=headers, params=querystring)
 
     bib_data = response.json()
 
     bib_url = bib_data["file"]
 
-    marc_response = requests.request("GET", bib_url, headers=headers)
+    marc_response = requests.get(bib_url, headers=headers)
 
     return marc_response.text
-    #full_marc = marc_response.json()
